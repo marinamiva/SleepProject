ca7bdaa19708082cea0ec8b9d2903ef78edebdb1
gabri <gabri@LAPTOP-3BH11PJV>
2021-11-30 16:42:43 +0100

    uploading sql and jar

--- OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/Database/DBManager.java
+++ OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/Database/DBManager.java
@@ -10,188 +10,180 @@ import Client.User;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class DBManager implements DBManagerInterface {
 
-	private Connection c;
-	private PatientManager patient;
+    public static void main(String[] args) {
+        DBManager dbmanager = new DBManager();
+        dbmanager.connect();
+    }       
+    
+    private Connection c;
+    private PatientManager patient;
 
-	public DBManager() {
-		super();
-	}
+    public DBManager() {
+        super();
+    }
 
-	public void connect() {
-		try {
-			// Open database connection
-			Class.forName("org.sqlite.JDBC");
-                                                //String url = "jdbc:sqlite:lib/db/SleepControlDB.db";
-			c = DriverManager.getConnection("jdbc:sqlite:lib/db/SleepControlDB.db");
-                        
-			c.createStatement().execute("PRAGMA foreign_keys=ON");
-			
-			// create PatientManager
-			patient = new PatientManager(c);
-                                                
+    public void connect() {
+        try {
+            // Open database connection
+            //System.out.println("donde estoy");
+          //Class.forName("org.sqlite.JDBC");
+          //Class.forName("org.sqlite.Driver");
+            String url = "jdbc:sqlite:lib/db/SleepControlDB.db";
+            c = DriverManager.getConnection(url);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            c.createStatement().execute("PRAGMA foreign_keys=ON");
 
-	public void setC(Connection c) {
-		this.c = c;
-	}
+            // create PatientManager
+            patient = new PatientManager(c);
 
-	public void disconnect() {
-		try {
-			// Close database connection
-			c.close();
-			// System.out.println("Database connection closed.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    public void setC(Connection c) {
+        this.c = c;
+    }
 
-	public void createTables() {
-		try {
-			// Open database connection
-			//Class.forName("org.sqlite.JDBC");
-			//DriverManager.getConnection("jdbc:sqlite:./db/SleepControl_DB.db");
-			//c.createStatement().execute("PRAGMA foreign_keys=ON");
-			System.out.println("Database connection opened.");
-			connect();
+    public void disconnect() {
+        try {
+            // Close database connection
+            c.close();
+            // System.out.println("Database connection closed.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			Statement stmt1 = c.createStatement();
-			String sql1 = "CREATE TABLE Patients " 
-					+ " (patient_id	INTEGER PRIMARY KEY AUTOINCREMENT," 
-					+ "NAME TEXT,"
-					+ "LASTNAME TEXT,"
-					+ "TELEPHONE TEXT," 
-					+ "ADDRESS TEXT," 
-                                        + "DNI TEXT,"
-                                        + "GENDER TEXT";
+    }
 
-			stmt1.executeUpdate(sql1);
-			stmt1.close();
+    public void createTables() {
+        try {
+            // Open database connection
+            //Class.forName("org.sqlite.JDBC");
+            //DriverManager.getConnection("jdbc:sqlite:./db/SleepControl_DB.db");
+            //c.createStatement().execute("PRAGMA foreign_keys=ON");
+            System.out.println("Database connection opened.");
+            connect();
 
-			Statement stmt2 = c.createStatement();
-			String sql2 = "CREATE TABLE Reports " 
-					+ "(patient_id	INTEGER PRIMARY KEY AUTOINCREMENT," 
-					+ "PATIENT_DNI TEXT,"
-					+ "REPORT_DATE DATE," 
-					+ "QUALITY TEXT,"+ "EXHAUSTION TEXT," 
-                                        + "HOURS TEXT,"
-                                        +"MOVEMENT TEXT,"+"TIME TEXT,"
-                                        +"REST TEXT,"+"AWAKE TEXT,"+"TIMES AWAKE"+"DREAMS TEXT,"+"WORRIES TEXT,"+"MOOD TEXT,"+"DOUBTS TEXT)";
-  
-			stmt2.executeUpdate(sql2);
-			stmt2.close();
+            Statement stmt1 = c.createStatement();
+            String sql1 = "CREATE TABLE Patients "
+                    + " (patient_id	INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "NAME TEXT,"
+                    + "LASTNAME TEXT,"
+                    + "TELEPHONE TEXT,"
+                    + "ADDRESS TEXT,"
+                    + "DOB DATE"
+                    + "DNI TEXT,"
+                    + "GENDER TEXT";
 
+            stmt1.executeUpdate(sql1);
+            stmt1.close();
 
-			Statement stmt3 = c.createStatement();
-			String sql3 = "CREATE TABLE EEGs " 
-					+ "(patient_id INTEGER PRIMARY KEY AUTOINCREMENT," 
-					+ "PATIENT_DNI TEXT,"
-					+ "EEG_DATE DATE," 
-					+ "EEG TEXT)";
+            Statement stmt2 = c.createStatement();
+            String sql2 = "CREATE TABLE Reports "
+                    + "(patient_id	INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "PATIENT_DNI TEXT,"
+                    + "REPORT_DATE DATE,"
+                    + "QUALITY TEXT," + "EXHAUSTION TEXT,"
+                    + "HOURS TEXT,"
+                    + "MOVEMENT TEXT," + "TIME TEXT,"
+                    + "REST TEXT," + "AWAKE TEXT," + "TIMES AWAKE" + "DREAMS TEXT," + "WORRIES TEXT," + "MOOD TEXT," + "DOUBTS TEXT)";
 
-			stmt3.executeUpdate(sql3);
-			stmt3.close();
-                        Statement stmt4 = c.createStatement();
-			String sql4 = "CREATE TABLE Users " 
-					+ "(patient_id INTEGER PRIMARY KEY AUTOINCREMENT," 
-					+ "PATIENT_DNI TEXT,"
-                                        + "Password";
+            stmt2.executeUpdate(sql2);
+            stmt2.close();
 
-			stmt4.executeUpdate(sql4);
-			stmt4.close();
+            Statement stmt3 = c.createStatement();
+            String sql3 = "CREATE TABLE EEGs "
+                    + "(patient_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "PATIENT_DNI TEXT,"
+                    + "EEG_DATE DATE,"
+                    + "EEG TEXT)";
 
-			System.out.println("Tables created.");
-			// Create table: end
-			//
-			// - Set initial values for the Primary Keys
-			// - Don't try to understand this until JPA is explained
-			// This is usually not needed, since the initial values
-			// are set when the first row is inserted, but since we
-			// are using JPA and JDBC in the same project, and JPA
-			// needs an initial value, we do this.
-			
-			Statement stmtSeq = c.createStatement();
-			String sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES('Patients', 1)";
-			stmtSeq.executeUpdate(sqlSeq);
-			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('Reports', 1)";
-			stmtSeq.executeUpdate(sqlSeq);
-			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('EEGs', 1)";
-			stmtSeq.executeUpdate(sqlSeq);
-			stmtSeq.close();
-			
-		
+            stmt3.executeUpdate(sql3);
+            stmt3.close();
+            Statement stmt4 = c.createStatement();
+            String sql4 = "CREATE TABLE Users "
+                    + "(patient_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "PATIENT_DNI TEXT,"
+                    + "Password";
 
-			// Close database connection
-			// c.close();
-			// System.out.println("Database connection closed.");
-	
-			
-			
-			
-		} catch (SQLException e) {
-			if (e.getMessage().contains("already exists")) {
-			}else{
-				e.printStackTrace();
-			}
-		}
-	}
+            stmt4.executeUpdate(sql4);
+            stmt4.close();
 
-	
-        @Override
-	public PatientManager getPatientManager() {
-		return patient;
-	}
-	
+            System.out.println("Tables created.");
+            // Create table: end
+            //
+            // - Set initial values for the Primary Keys
+            // - Don't try to understand this until JPA is explained
+            // This is usually not needed, since the initial values
+            // are set when the first row is inserted, but since we
+            // are using JPA and JDBC in the same project, and JPA
+            // needs an initial value, we do this.
 
-	@Override
-	public int getLastId() {
-		int result = 0;
-		try {
-			String query = "SELECT last_insert_rowid() AS lastId";
-			PreparedStatement p = c.prepareStatement(query);
-			ResultSet rs = p.executeQuery();
-			result = rs.getInt("lastId");
+            Statement stmtSeq = c.createStatement();
+            String sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES('Patients', 1)";
+            stmtSeq.executeUpdate(sqlSeq);
+            sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('Reports', 1)";
+            stmtSeq.executeUpdate(sqlSeq);
+            sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('EEGs', 1)";
+            stmtSeq.executeUpdate(sqlSeq);
+            stmtSeq.close();
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	
+            // Close database connection
+            // c.close();
+            // System.out.println("Database connection closed.");
+        } catch (SQLException e) {
+            if (e.getMessage().contains("already exists")) {
+            } else {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public void deleteTables() {
-		try {
+    @Override
+    public PatientManager getPatientManager() {
+        return patient;
+    }
 
-			// Drop tables: begin
-			
-			Statement stmt2 = c.createStatement();
-			String sql2 = "DROP TABLE Patients";
-			stmt2.executeUpdate(sql2);
-			stmt2.close();
-			Statement stmt3 = c.createStatement();
-			String sql3 = "DROP TABLE Reports";
-			stmt3.executeUpdate(sql3);
-			stmt3.close();
-			Statement stmt4 = c.createStatement();
-			String sql4 = "DROP TABLE EEGs";
-			stmt4.executeUpdate(sql4);
-			stmt4.close();
-			System.out.println("Tables removed.");
-			// Drop tables: end
+    @Override
+    public int getLastId() {
+        int result = 0;
+        try {
+            String query = "SELECT last_insert_rowid() AS lastId";
+            PreparedStatement p = c.prepareStatement(query);
+            ResultSet rs = p.executeQuery();
+            result = rs.getInt("lastId");
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
+    public void deleteTables() {
+        try {
+
+            // Drop tables: begin
+            Statement stmt2 = c.createStatement();
+            String sql2 = "DROP TABLE Patients";
+            stmt2.executeUpdate(sql2);
+            stmt2.close();
+            Statement stmt3 = c.createStatement();
+            String sql3 = "DROP TABLE Reports";
+            stmt3.executeUpdate(sql3);
+            stmt3.close();
+            Statement stmt4 = c.createStatement();
+            String sql4 = "DROP TABLE EEGs";
+            stmt4.executeUpdate(sql4);
+            stmt4.close();
+            System.out.println("Tables removed.");
+            // Drop tables: end
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
-
--- OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/Database/Menu.java
+++ OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/Database/Menu.java
@@ -17,64 +17,56 @@ import java.text.SimpleDateFormat;
  *
  * @author gabri
  */
-
 public class Menu {
+
     private static DBManagerInterface dbm;
     private static Database.DBManagerInterface dbman;
-    private static PatientManagerInterface pmi; 
+    private static PatientManagerInterface pmi;
     private static UserManagerInterface umi;
     private static BufferedReader br;
-    
-    
+
     public static void main(String[] args) throws IOException, ParseException {
-       
-        dbman=new DBManager();
-        dbm.connect();
-        dbm.createTables();
-        
-        br = new BufferedReader (new InputStreamReader(System.in));
-        
+
+        dbman = new DBManager();
+        dbman.connect();
+        //dbm.createTables();
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Hello, type the option you want: 1. Add patient, 2: Todavia no");
         int number = sc.nextInt();
-        
-        switch(number){
-            
-            case 1: 
+
+        switch (number) {
+
+            case 1:
                 addPatient();
                 break;
         }
-        
+
     }
-        
-        public static void  addPatient() throws IOException, ParseException {
-            System.out.println("Type the name of the patient you'll add");
-            String name = br.readLine();
-            System.out.println("Type the lastname of the patient:");
-             String lastname = br.readLine();
-             System.out.println("Type the telephone of the patient");
-             String telephone = br.readLine();
-             System.out.println("Type the address of the patient");
-             String address = br.readLine();
-             System.out.println("Type the Date of Birth of the patient followed by /");
-             String date = br.readLine();
-              java.util.Date dateBirth = new SimpleDateFormat().parse(date);
-              java.sql.Date dob = (java.sql.Date) dateBirth;
-             System.out.println("Type the DNI of the patient");
-             String dni = br.readLine();
-             System.out.println("Type the gender of the patient: ");
-             String gender = br.readLine();
-             
-             Patient newpat = new Patient(name, lastname, telephone,address, dob,dni, gender);
-             pmi.addpatientbyRegister(newpat);
-        }
-        
+
+    public static void addPatient() throws IOException, ParseException {
+        System.out.println("Type the name of the patient you'll add");
+        String name = br.readLine();
+        System.out.println("Type the lastname of the patient:");
+        String lastname = br.readLine();
+        System.out.println("Type the telephone of the patient");
+        String telephone = br.readLine();
+        System.out.println("Type the address of the patient");
+        String address = br.readLine();
+        System.out.println("Type the Date of Birth of the patient followed by /");
+        String date = br.readLine();
+        java.util.Date dateBirth = new SimpleDateFormat().parse(date);
+        java.sql.Date dob = (java.sql.Date) dateBirth;
+        System.out.println("Type the DNI of the patient");
+        String dni = br.readLine();
+        System.out.println("Type the gender of the patient: ");
+        String gender = br.readLine();
+
+        Patient newpat = new Patient(name, lastname, telephone, address, dob, dni, gender);
+        pmi.addpatientbyRegister(newpat);
     }
-    
 
-
-
-
-    
-
+}
