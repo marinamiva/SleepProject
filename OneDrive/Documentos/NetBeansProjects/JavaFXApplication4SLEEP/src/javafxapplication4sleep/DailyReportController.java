458ee8fe65277438c522367e8608405a8742da60
mmsantaolalla <m.montes17@usp.ceu.es>
2021-12-05 17:37:43 +0100

    New Daily Report and connection between windows

--- OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/javafxapplication4sleep/DailyReportController.java
+++ OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/javafxapplication4sleep/DailyReportController.java
@@ -5,6 +5,7 @@
  */
 package javafxapplication4sleep;
 
+import Client.Report;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
@@ -26,14 +27,56 @@ import javafx.stage.Stage;
 
 
 /**
- * FXML Controller class
  *
- * @author gabri
+ * @author mmsan
  */
 public class DailyReportController implements Initializable {
     
-
-      public void start(Stage primaryStage) throws Exception {
+    @FXML private Label patientDailyReport;
+    @FXML private TextField Answer1;
+    @FXML private TextField Answer2;
+    @FXML private TextField Answer3;
+    @FXML private TextField Answer4;
+    @FXML private TextField Answer5;
+    @FXML private TextField Answer6;
+    @FXML private TextField Answer7;
+    @FXML private TextField Answer8;
+    @FXML private TextField Answer9;
+    @FXML private TextField Answer10;
+    @FXML private TextField Answer11;
+    @FXML private TextField Doughts;
+    @FXML private Button save;
+    @FXML private Button back;
+    
+    String dateActual, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, doughts;
+    Date date;
+    
+     @FXML void actionSave (javafx.event.ActionEvent evento) throws ParseException {
+        answer1=Answer1.getText();
+        answer2=Answer2.getText();
+        answer3=Answer3.getText();
+        answer4=Answer4.getText();
+        answer5=Answer5.getText();
+        answer6=Answer6.getText();
+        answer7=Answer7.getText();
+        answer8=Answer8.getText();
+        answer9=Answer9.getText();
+        answer10=Answer10.getText();
+        answer11=Answer11.getText();
+        doughts=Doughts.getText();
+        dateActual = actualDate();
+        SimpleDateFormat formatFinalDate = new SimpleDateFormat("dd/MM/yyyy");
+        date=formatFinalDate.parse(dateActual) ;
+       
+        
+        Report report1 = new Report (date,answer1,answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, doughts);
+        
+        
+    
+     //String sleepQuality, String exhaustion,String averageHours,String movement,String timeToFallAsleep,String rest,String stayAwake, String timesAwake, String dreams, String worries, String todaysMood, String doubtsForDoctor
+     }
+     
+    public void start(Stage primaryStage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getResource("DailyReport.fxml"));
         
@@ -42,53 +85,29 @@ public class DailyReportController implements Initializable {
         primaryStage.setTitle("DAILY REPORT");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
+    }      
+ 
     
-    
+     public void goBackClientMenu (javafx.event.ActionEvent event) throws IOException{
+           Parent root = FXMLLoader.load(getClass().getResource("OptionsMenu.fxml"));
+        
+        Scene clientMenu = new Scene(root);
+        
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(clientMenu);
+        window.show();
+     }
+     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
-    @FXML
-    private void goBackClientMenu(javafx.event.ActionEvent event) throws IOException {
-                    
-        Parent root = FXMLLoader.load(getClass().getResource("OptionsMenu.fxml"));
-        
-        Scene reportHistory = new Scene(root);
-        
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        
-        window.setScene(reportHistory);
-        window.show();
-    }
-    /*
-        String sleepQuality, exhaustion, averageHours, movement, timeToFallAsleep, rest, stayAwake, timesAwake, dreams, worries, todaysMood, doubtsForDoctor;
-    Date date;//no lleva a nada, preguntar alberto
     
-    @FXML void saveReport (ActionEvent evento) throws ParseException {
+    public static String actualDate(){
+        Date actual = new Date();
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
         
-        date = new Date();
-        
-        // new label for date
-        // Casting de date a String
-        // dateLabel.setText("STRING QUE TENGO EN LA VARIABLE ANTERIOR");
-        sleepQuality=answer1.getText();
-        exhaustion=answer2.getText();
-        averageHours=answer3.getText();
-        movement=answer4.getText();
-        timeToFallAsleep=answer5.getText();
-        rest=answer6.getText();
-        stayAwake=answer7.getText();
-        timesAwake=answer8.getText();
-        dreams=answer9.getText();
-        worries=answer10.getText();
-        todaysMood=answer11.getText();
-        doubtsForDoctor=doughts.getText();
-        
-        
-        Report report1 = new Report (date,sleepQuality, exhaustion, averageHours, movement, timeToFallAsleep, rest, stayAwake, timesAwake, dreams, worries, todaysMood, doubtsForDoctor);
-        
+        return formatDate.format(actual);
     }
-    */
 }
