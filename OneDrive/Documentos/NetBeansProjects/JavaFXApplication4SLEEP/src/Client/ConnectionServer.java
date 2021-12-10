075e906b394b465592a5c775f90ba678b6812b67
mariaceleste <mariaceleste@localhost>
2021-12-10 12:48:28 +0100

    testing the connection client and server to send patients, files and reports

--- OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/Client/ConnectionServer.java
+++ OneDrive/Documentos/NetBeansProjects/JavaFXApplication4SLEEP/src/Client/ConnectionServer.java
@@ -18,6 +18,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConnectionServer {
+
+    private Connection c;
+    private static Database.DBManagerInterface dbman;
+    private static PatientManagerInterface pmi;
+    private static BufferedReader br;
     public static void main(String[] args) throws ClassNotFoundException, ParseException, UnknownHostException, IOException {
         dbman = new DBManager();
         dbman.connect();
@@ -25,44 +30,41 @@ public class ConnectionServer {
         br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
         int max;
-        int num,numUsing;
+        int num, numUsing;
         br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("MENU CLIENT TO SELECT CONNECTION");
         System.out.println("First, write your IP: ");
-        String ipString=sc.next();
+        String ipString = sc.next();
         InetAddress ip = InetAddress.getByName(ipString);
-        String dni = ui.takeDNI(br, "Write the dni of the data you want to send:");
-        System.out.println("\nWhat do you want to do?\n"+"1.Send patient.\n"+"2.Send Report.\n"+"3.Send File with EEG values.\n");
-        max=3;
+        String dni = ui.takeDNI(br, "Write the patient DNI of the data you want to send:");
+        System.out.println("\nWhat do you want to do?\n" + "1.Send patient.\n" + "2.Send Report.\n" + "3.Send File with EEG values.\n");
+        max = 3;
         Patient pat = pmi.searchSpecificPatientByDNI(dni);
         Report rep = pmi.getReportByDni(dni);
         System.out.println("0. Exit.\n");
-        num=requestNumber(max);
-        numUsing=num;
-        switch(num){
+        num = requestNumber(max);
+        numUsing = num;
+        switch (num) {
             case 1:
-                sendPatient(pat,ip);
+                sendPatient(pat, ip);
                 break;
             case 2:
-                sendReport(rep,ip);
+                sendReport(rep, ip);
                 break;
             case 3:
-                sendFile(pat,ip);
+                sendFile(pat, ip);
                 break;
             case 0:
                 System.exit(0);
                 break;
-            }
-            
-            
+        }
 
     }
 
-    
-    
     /**
      * Sendind an object patient through a socket to a server.
+     *
      * @param patient
      * @param ip
      */
@@ -70,20 +72,20 @@ public class ConnectionServer {
         Socket socketSender = null;
         PrintWriter print = null;
         SimpleDateFormat formato;
-        String total, name, lastName, telephone, address, date, dni,gender ;
+        String total, name, lastName, telephone, address, date, dni, gender;
 
         try {
             socketSender = new Socket(ip, 9010);
             print = new PrintWriter(socketSender.getOutputStream(), true);
-            name=patient.getName();
-            lastName=patient.getLastname();
-            telephone=patient.getTelephone();
-            address=patient.getAddress();
+            name = patient.getName();
+            lastName = patient.getLastname();
+            telephone = patient.getTelephone();
+            address = patient.getAddress();
             formato = new SimpleDateFormat("yyyy-MM-dd");
             date = formato.format(patient.getDateOfBirth());
-            dni=patient.getDni();
-            gender= patient.getGender();
-            total="\n PATIENT'S INFORMATION \n Name: "+name+"\n Lastname: "+lastName+"\n Telephone: "+telephone+"\n Address: "+address+"\n Date of birth: "+date+"\n DNI: "+dni+"\n Gender: "+gender+"\n"+"\n finish";
+            dni = patient.getDni();
+            gender = patient.getGender();
+            total = "\n PATIENT'S INFORMATION \n Name: " + name + "\n Lastname: " + lastName + "\n Telephone: " + telephone + "\n Address: " + address + "\n Date of birth: " + date + "\n DNI: " + dni + "\n Gender: " + gender + "\n" + "\n finish";
             print.println(total);
             releaseResources(print, socketSender);
         } catch (IOException io) {
@@ -94,6 +96,7 @@ public class ConnectionServer {
 
     /**
      * Sending an object Report through a socket to a server.
+     *
      * @param rep
      * @param ip
      */
@@ -105,24 +108,24 @@ public class ConnectionServer {
         try {
             socketSender = new Socket(ip, 9010);
             print = new PrintWriter(socketSender.getOutputStream(), true);
-            String dni,date,sleepqual,exhaus,average,movement,timeToFall,rest,stayAwake,timesAwake,dreams,worries,todaysMood,doubtsForDoctor;
+            String dni, date, sleepqual, exhaus, average, movement, timeToFall, rest, stayAwake, timesAwake, dreams, worries, todaysMood, doubtsForDoctor;
             String total;
             SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
             date = formato.format(rep.getTodaysDate());
-            dni=rep.getPatdni();
-            sleepqual=rep.getsleepQuality();
-            exhaus=rep.getExhaustion();
-            average=rep.getAverageHours();
-            movement=rep.getMovement();
-            timeToFall= rep.gettimeToFallAsleep();
-            rest=rep.getRest();
-            stayAwake=rep.getStayAwake();
-            timesAwake= rep.getTimesAwake();
-            dreams=rep.getDreams();
-            worries=rep.getWorries();
-            todaysMood= rep.getTodaysMood();
-            doubtsForDoctor=rep.getdoubtsForDoctor();
-            total="\n Report's date: " +date+"\n DNI: "+dni+"\n Sleep quality: "+sleepqual+"\n Exhaustion: "+exhaus+"\n Average hours: "+average+"\n Movement: "+movement+"\n Time to fall asleep: "+timeToFall+"\n Have you rest? "+rest+"\n Do you stay awake all day? "+stayAwake+"\n How many times did you wake up in the night? "+timesAwake+"\n Have you dreamt? "+dreams+"\n Worries: "+worries+"\n Todays mood: "+todaysMood+"\n Doubts: "+doubtsForDoctor+"\n"+"finish";
+            dni = rep.getPatdni();
+            sleepqual = rep.getsleepQuality();
+            exhaus = rep.getExhaustion();
+            average = rep.getAverageHours();
+            movement = rep.getMovement();
+            timeToFall = rep.gettimeToFallAsleep();
+            rest = rep.getRest();
+            stayAwake = rep.getStayAwake();
+            timesAwake = rep.getTimesAwake();
+            dreams = rep.getDreams();
+            worries = rep.getWorries();
+            todaysMood = rep.getTodaysMood();
+            doubtsForDoctor = rep.getdoubtsForDoctor();
+            total = "\n Report's date: " + date + "\n DNI: " + dni + "\n Sleep quality: " + sleepqual + "\n Exhaustion: " + exhaus + "\n Average hours: " + average + "\n Movement: " + movement + "\n Time to fall asleep: " + timeToFall + "\n Have you rest? " + rest + "\n Do you stay awake all day? " + stayAwake + "\n How many times did you wake up in the night? " + timesAwake + "\n Have you dreamt? " + dreams + "\n Worries: " + worries + "\n Todays mood: " + todaysMood + "\n Doubts: " + doubtsForDoctor + "\n" + "finish";
             print.println(total);
             releaseResources(print, socketSender);
         } catch (IOException io) {
@@ -132,12 +135,14 @@ public class ConnectionServer {
     }
 
     /**
-     * Sending a File with the information about EEG of a patient, its name and the time of the EEG through a socket to a server.
+     * Sending a File with the information about EEG of a patient, its name and
+     * the time of the EEG through a socket to a server.
+     *
      * @param file
      * @param ip
      */
     public static void sendFile(Patient pat, InetAddress ip) {
-        File file = new File("./recordedSignal_"+pat.getDni()+".txt"); 
+        File file = new File("./recordedSignal_" + pat.getDni() + ".txt");
 
         try {
             Socket socket = new Socket(ip, 9010);
@@ -149,21 +154,15 @@ public class ConnectionServer {
                 outputStream.flush();
 
             }
-         outputStream.flush();
-        releaseResources(outputStream, br, socket);
+            outputStream.flush();
+            releaseResources(outputStream, br, socket);
         } catch (IOException ex) {
             System.out.println("No possible to connect.");
             Logger.getLogger(ConnectionServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    private Connection c;
-    private static Database.DBManagerInterface dbman;
-    private static PatientManagerInterface pmi;
-    private static BufferedReader br;
 
-      
-    
     private static void releaseResources(PrintWriter p, Socket socket) {
         try {
             p.close();
@@ -173,17 +172,18 @@ public class ConnectionServer {
         }
 
     }
+
     public static int requestNumber(int max) {
-		 br = new BufferedReader(new InputStreamReader(System.in));
-                int num;
-		do {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int num;
+        do {
 
-			num = ui.takeInteger(br, "Introduce the number: ");
+            num = ui.takeInteger(br, "Introduce the number: ");
 
-		} while (ui.CheckOption(num, max));
+        } while (ui.CheckOption(num, max));
 
-		return num;
-	}
+        return num;
+    }
 
     private static void releaseResources(OutputStream outputStream,
             BufferedReader br, Socket socket) {
